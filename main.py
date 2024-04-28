69c67d33c20f5aa2aa55a12ef091c04b2bcdb5cc
iamujjalsurroy <156598120+iamujjalsurroy@users.noreply.github.com>
2024-04-28 14:54:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,7 @@
-from flask import Flask, request, jsonify
-from pymongo import MongoClient
-import json 
-from bson import json_util
+from flask import Flask
 
 app = Flask(__name__)
-cluster = MongoClient("mongodb+srv://rovonod596:qDHxMuczWqT4jJfv@cluster0.c0szyw0.mongodb.net/")
-db = cluster["basic_app"]
-collection = db["credentials"]
 
-# collection.insert_one({"seed": "check"})
-
-# @app.route("/add_login_info", methods=["POST"])
-# def add_login_info():
-#     data = request.json
-#     if data:
-#         collection.insert_one(data)
-#         return jsonify({"message": "Data added successfully"}), 201
-#     else:
-#         return jsonify({"error": "No data provided"}), 400
-
-@app.route("/login_info", methods=["GET"])
-def get_login_details():
-    all_details = list(collection.find())
-    return json.dumps(all_details, default=json_util.default)
-
-# if __name__ == "__main__":
-#     app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 80)))
+@app.route("/")
+def hello_world():
+    return "Hello World!"
